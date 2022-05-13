dc0de44276f1c75952d690a78103e35fd922e4b4
BahaaNimer <bahaanimer97@gmail.com>
2022-05-13 03:10:56 +0300

    make .env port

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-const PORT = 3001 || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Raouts:
 
